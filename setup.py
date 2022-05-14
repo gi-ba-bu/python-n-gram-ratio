699514893ee194ddef03d00d25d36fd4740d7fcf
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-05-14 21:15:14 +0200

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ngramratio",
-    version="0.0.4",
+    version="0.0.5",
     author="Giacomo Baldo",
     author_email="baldogiacomophd@gmail.com",
     description="N-grams based similarity score",
